bff190659243fcbae55eb496af704c3b96ebd933
Kyaligonza <sarah.kyaligonza@gmail.com>
2018-12-10 17:21:34 -0800

    updatedwithencrycomplex

--- encryptor/encrycomplex/sub.py
+++ encryptor/encrycomplex/sub.py
@@ -4,7 +4,10 @@ according to the key. This kind of encryption is called Substitution Encryption.
 - The module also decrypts the message given that we know the key value of the encryption."""
 
 
-import random
+try:
+    import random
+except ImportError:
+    print("The import function did not work!!")
 class Suben:
     def __init__(self,text,key):
         self.text = text
@@ -12,30 +15,40 @@ class Suben:
         
 class Encr(Suben):
     def __init__(self,text,key):
-        Suben.__init__(self,text,key)
-        self.e = ''
-        for c in text :
-            if ord(c) != 32:
-                if ord(c) == 122 :
-                    self.e += 'a'
-                else :
-                    self.e += chr(ord(c)+key)
-            elif ord(c) == 32:
-                self.e += ' '
+        try:
+            Suben.__init__(self,text,key)
+            self.e = ''
+            for c in text :
+                if ord(c) != 32:
+                    if ord(c) == 122 :
+                        self.e += 'a'
+                    else :
+                        self.e += chr(ord(c)+key)
+                elif ord(c) == 32:
+                    self.e += ' '
+        except TypeError:
+            print("Pass a string in the first argument!!")
+        else:
+            print("Successful!!!")
     def display(self):
         return self.e
     
 class Decr(Suben):
     def __init__(self,text,key):
-        Suben.__init__(self,text,key)
-        self.e = ''
-        for c in text :
-            if ord(c) != 32:
-                if ord(c) == 122 :
-                    self.e += 'a'
-                else :
-                    self.e += chr(ord(c)-key)
-            elif ord(c) == 32:
-                self.e += ' '
+        try:
+            Suben.__init__(self,text,key)
+            self.e = ''
+            for c in text :
+                if ord(c) != 32:
+                    if ord(c) == 122 :
+                        self.e += 'a'
+                    else :
+                        self.e += chr(ord(c)-key)
+                elif ord(c) == 32:
+                    self.e += ' '
+        except TypeError:
+            print("Pass a string in the first argument!!")
+        else:
+            print("Successful!!!")   
     def display(self):
         return self.e
--- encryptor/encrycomplex/tran.py
+++ encryptor/encrycomplex/tran.py
@@ -1,11 +1,20 @@
 """The module encrypts the message by jumbling up the letters in the message text letter by letter. This kind of encryption
 is called Transposition Encryption."""
-
-import random
+try:
+    import random
+except ImportError:
+    print("The function did not work!!")
+else:
+    print("Succfully imported the module!!")
 def tran(a):
-    random.seed(1001)
-    e = ''
-    i = random.sample(range(len(a)),len(a))
-    for index in i:
-        e += a[index]
-    return e
+    try:
+        random.seed(1001)
+        e = ''
+        i = random.sample(range(len(a)),len(a))
+        for index in i:
+            e += a[index]
+        return e
+    except TypeError:
+        print("Pass a string in the argument!!")
+    else:
+        print("Success!!")
